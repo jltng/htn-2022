0dc9e9f3022700074e1e93b4f39a103635ab8bac
Julie Tang <84947592+jltng@users.noreply.github.com>
2022-09-18 04:02:22 -0400

    to pubspecs

---
+++ pick-genre/lib/genre_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'newgenre.dart';
+
+//
+// class GenrePage extends StatefulWidget {
+//   const GenrePage({Key? key}) : super(key: key);
+
+//   @override
+//   State<GenrePage> createState() => _GenrePageState();
+// }
+
+// class _GenrePageState extends State<GenrePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white70,
+//       // body: SafeArea(
+//       //   child: Padding(
+//       //     padding: const EdgeInsets.symmetric(horizontal: 25.0),
+//       //     child: Column(
+//       //       children: [
+//       //         // back + home buttons
+//       //         Row(
+//       //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       //           children: [
+//       //             SizedBox(
+//       //               height: 40,
+//       //               width: 40,
+//       //               child: NewBox(child: Icon(Icons.arrow_back)),
+//       //             ),
+//       //             Text(
+//       //               'Select a genre/playlist:',
+//       //               style: TextStyle(
+//       //                 fontWeight: FontWeight.bold,
+//       //                 fontSize: 20,
+//       //                 color: Color(0xff7f31a7),
+//       //               ),
+//       //             ),
+//       //             SizedBox(
+//       //               height: 40,
+//       //               width: 40,
+//       //               child: NewBox(child: Icon(Icons.home)),
+//       //             )
+//       //           ],
+//       //         ),
+//       //
+//       //         const SizedBox(height: 15),
+//       //
+//       //         // album
+//       //         NewBox(
+//       //           child: Column(
+//       //             children: [
+//       //               ClipRRect(
+//       //                 borderRadius: BorderRadius.circular(8),
+//       //                 child: Image(
+//       //                   image: AssetImage('lib/images/rb.png'),
+//       //                   fit: BoxFit.fill,
+//       //                 ),
+//       //               ),
+//       //               Padding(
+//       //                 padding: const EdgeInsets.all(2.0),
+//       //                 child: Row(
+//       //                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       //                   children: [
+//       //                     Column(
+//       //                       crossAxisAlignment: CrossAxisAlignment.start,
+//       //                       children: [
+//       //                         Text('r & b'),
+//       //                       ],
+//       //                     )
+//       //                   ],
+//       //                 ),
+//       //               )
+//       //             ],
+//       //           ),
+//       //         ),
+//       //       ],
+//       //     ),
+//       //   ),
+//       // ),
+//       );
+//     ),
+//   }
+// }
